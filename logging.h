0246e847767b421daa9a31ca10ba231143798ad3
zx013 <1453145267@qq.com>
2018-03-13 17:02:45 +0800

    log print type

--- logging.h
+++ logging.h
@@ -24,6 +24,14 @@ static const char *LOGGING_LEVEL[] =
 	"CRITICAL"
 };
 
+/*
+日志打印时，将所有错误等级的日志保存在一定大小的共享内存队列中，当有数据从队列中移出时，判断日志打印等级确定该日志是否需要打印。当出现错误时（LOGGING_ERROR，LOGGING_CRITICAL），降低打印等级到LOGGING_NOTSET使所有日志全部打印。直到连续若干条日志记录没有错误后，恢复日志等级。
+当进程退出时，将队列中的日志全部写入文件。
+可提供参数如print-all等，再次运行程序./zx013.exe --print-all将所有日志打印
+print-immediately，所有产生的日志立即打印
+同时也可以根据其他参数在外部动态修改日志打印配置
+*/
+
 void logging_init(char *filename);
 
 #define logging(message, level, ...) _logging(message, __FILE__, __FUNCTION__, __LINE__, level, ##__VA_ARGS__, NULL)
